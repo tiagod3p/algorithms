0c37177f9210735a9648a73db0370fe1de99bfe0
Tiago Vidal <tiagooliveiravidaldepaula@gmail.com>
2020-08-31 18:22:51 -0300

    feat: add function to insert values in a sorted vector

--- vectors/sorted.py
+++ vectors/sorted.py
@@ -6,3 +6,41 @@ class SortedVector:
         self.size = size
         self.last_position = -1
         self.values = np.empty(self.size, dtype=int)
+
+    def print_elements(self):
+        if self.last_position == -1:
+            print("Vector empty.")
+        else:
+            for i in range(self.last_position + 1):
+                print(f"Position: {i} ---- Value: {self.values[i]}")
+
+    # Big O ---> O(N)
+    def insert(self, value):
+        if self.last_position + 1 == self.size:
+            print("Vector full.")
+            return
+
+        position = 0
+        for i in range(self.last_position + 1):
+            position = i
+            if self.values[i] > value:
+                break
+            if position == self.last_position:
+                position = i + 1
+
+        j = self.last_position
+        while j >= position:
+            self.values[j + 1] = self.values[j]
+            j -= 1
+
+        self.values[position] = value
+        self.last_position += 1
+
+
+vector_1 = SortedVector(5)
+vector_1.print_elements()
+vector_1.insert(10)
+vector_1.insert(7)
+vector_1.insert(8)
+vector_1.insert(1)
+vector_1.insert(50)
